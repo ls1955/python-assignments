204b2941ebc2b78e46cd047ee29c5ce0fa4ec8b4
Cheong <xxlsxx1955@gmail.com>
2024-04-08 10:34:00 +0800

    Include is_valid_address function
    This commit also include related assertions.

--- part_b.py
+++ part_b.py
@@ -1,8 +1,12 @@
 import re
 
 def is_valid_price(price: str) -> bool:
-    VALID_PATTERN = r"RM(\d{1,3},)*\d+(\.\d{2})?$"
+    VALID_PATTERN = r"^RM(\d{1,3},)*\d+(\.\d{2})?$"
     return bool(re.match(VALID_PATTERN, price))
+
+def is_valid_address(address: str) -> bool:
+    VALID_PATTERN = r"^\d+ ([A-Z][a-zA-Z]+\.? )*([a-zA-Z]+\.? )*[a-zA-Z]+\.?$"
+    return bool(re.match(VALID_PATTERN, address))
 
 if __name__ == "__main__":
     valid_prices = ["RM23", "RM23.00", "RM6000.99", "RM6,000.99", "RM6,888,222.99"]
@@ -11,4 +15,12 @@ if __name__ == "__main__":
     for price in valid_prices:
         assert is_valid_price(price), f"{price} should be valid"
     for price in invalid_prices:
-        assert not is_valid_price(price), f"{price} should not be valid"
+        assert not is_valid_price(price), f"{price} should be invalid"
+
+    valid_addresses = ["56 Jalan Mas Merah", "33 Kota tinggi", "77 J. Sungai Luas"]
+    invalid_addresses = ["Lot 8 Kampung Lapan, 8-A", "23 12 Taman baru", "13.8 Jalan Hang Tuah"]
+
+    for address in valid_addresses:
+        assert is_valid_address(address), f"{address} should be valid"
+    for address in invalid_addresses:
+        assert not is_valid_address(address), f"{address} should be invalid"
