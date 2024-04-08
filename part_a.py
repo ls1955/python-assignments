73774989ddb63f0fa65184043d4ce1ec6739f6e3
Cheong <xxlsxx1955@gmail.com>
2024-04-08 17:40:44 +0800

    Include default input value
    This commit also:
    * Insert prompt directly inside `input`
    * Fixed `G` value inside `GravitationalForceCalculator#__init__`

--- part_a.py
+++ part_a.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 class GravitationalForceCalculator:
-    def __init__(self, m1, m2, G=6.674 * 10**-11):
+    def __init__(self, m1, m2):
         self.m1 = m1
         self.m2 = m2
-        self.G = G
+        self.G = 6.674 * 10**-11
 
     def calculate_force(self, distance):
         return (self.G * self.m1 * self.m2) / (distance ** 2)
@@ -23,16 +23,10 @@ def plot_force_vs_distance(calculator, start=100, end=1000, interval=50):
     plt.show()
 
 if __name__ == "__main__":
-    # Collecting user inputs for masses of the two bodies
-    print("Enter the mass of the first body (in kg):")
-    m1 = float(input())
-    print("Enter the mass of the second body (in kg):")
-    m2 = float(input())
-    
-    # The gravitational constant G is set to a fixed value
-    G = 6.674 * 10**-11  # N*m^2/kg^2
-    
-    calculator = GravitationalForceCalculator(m1, m2, G)
-    
+    m1 = float(input("Enter m1 (kg): ").strip() or 0.5)
+    m2 = float(input("Enter m2 (kg): ").strip() or 1.5)
+
+    calculator = GravitationalForceCalculator(m1, m2)
+
     # Plot gravitational force from 100m to 1000m with intervals of 50m
     plot_force_vs_distance(calculator)
